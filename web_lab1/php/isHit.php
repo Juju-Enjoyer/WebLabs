42e7f9dcd63a17253a5de477693e824eb836be12
Juju-Enjoyer <115220987+Juju-Enjoyr@users.noreply.github.com>
2023-10-22 06:17:01 +0300

    Update isHit.php

--- web_lab1/php/isHit.php
+++ web_lab1/php/isHit.php
@@ -1,18 +1,16 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: X-Requested-With");
 
-require_once 'validation.php';
-date_default_timezone_set('Europe/Moscow');
-
-session_start();
+require_once "validation.php";
+date_default_timezone_set("Europe/Moscow");
 
 $startScriptTime = microtime(true);
 $currentTime = date("H:i:s");
 $x = $_GET["x"];
-$y = round((double)$_GET["y"], 2);
-$r = round((double)$_GET["r"], 2);
+$y = round((float) $_GET["y"], 2);
+$r = round((float) $_GET["r"], 2);
 
 if (!isValid($x, $y, $r)) {
     http_response_code(400);
@@ -20,20 +18,20 @@ if (!isValid($x, $y, $r)) {
     return;
 }
 
-$hitResult = isHit($x, $y, $r) ?
-    "True" :
-    "False";
+$hitResult = isHit($x, $y, $r) ? "True" : "False";
 
-$scriptExecutionTime = sprintf("%.3f", (microtime(true) - $startScriptTime) * 1000000);
+$scriptExecutionTime = sprintf(
+    "%.3f",
+    (microtime(true) - $startScriptTime) * 1000000
+);
 
-$receivedData = array(
+$receivedData = [
     "x" => $x,
     "y" => $y,
     "r" => $r,
     "time" => $currentTime,
     "execution_time" => $scriptExecutionTime,
-    "hit_result" => $hitResult
-);
+    "hit_result" => $hitResult,
+];
 
 echo json_encode($receivedData);
-?>
